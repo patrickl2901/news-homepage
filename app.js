68df7af5ce4387084a5e712911be61b0864faf58
patrickl2901 <patrickloos@gmx.net>
2023-02-15 15:54:25 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+const openMenu = () => {
+    document.getElementById("mobilenav").style.display="block";
+    console.log("opened menu");
+}
+
+const closeMenu = () => {
+    document.getElementById("mobilenav").style.display="none";
+    console.log("closed menu");
+}
